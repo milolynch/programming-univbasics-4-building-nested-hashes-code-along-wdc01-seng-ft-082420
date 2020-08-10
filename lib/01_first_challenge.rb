fc787d6be0ec5ce301e313c8edb7508f5872096d
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 20:03:16 +0000

    Done.

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,10 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       #first  level 2 hash
+     },
+     :capulet => {
+       #second level 2 hash
+     }
+   }
 end
